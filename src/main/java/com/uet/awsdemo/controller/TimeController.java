b505ee36a8e56c9ac400d258c8c80893ae9adb6b
Dang Thanh Trung <dttrung257@gmail.com>
2023-03-18 19:23:28 +0700

    aws demo api

---
+++ src/main/java/com/uet/awsdemo/controller/TimeController.java
@@ -0,0 +1,22 @@
+package com.uet.awsdemo.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class TimeController {
+	@GetMapping("/time")
+	public ResponseEntity<String> getTime() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		sdf.setTimeZone(TimeZone.getTimeZone("Asia/Saigon"));
+		String date = sdf.format(new Date());
+		return ResponseEntity.ok(date + "s");
+	}
+}
