818aa1407dc1a246eb091b78b5bdc5f8c99995e9
Dang Thanh Trung <dttrung257@gmail.com>
2023-03-19 11:50:08 +0700

    Update

--- src/main/java/com/uet/awsdemo/controller/TimeController.java
+++ src/main/java/com/uet/awsdemo/controller/TimeController.java
@@ -14,6 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class TimeController {
 	@GetMapping("/time")
 	public ResponseEntity<String> getTime() {
+		String str = "";
+		for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
+				for (int k = 0; k < 100; k++) {
+					str += "a";
+				}
+			}
+		}
 		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
 		sdf.setTimeZone(TimeZone.getTimeZone("Asia/Saigon"));
 		String date = sdf.format(new Date());
